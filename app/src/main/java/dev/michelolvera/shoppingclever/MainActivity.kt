4d506819e5fc18e5287fc1c78338f8173881ed50
Michel Olvera <michel.olvera.p@outlook.com>
2020-09-22 00:41:03 -0500

    Google and Anonymous Login.

--- app/src/main/java/dev/michelolvera/shoppingclever/MainActivity.kt
+++ app/src/main/java/dev/michelolvera/shoppingclever/MainActivity.kt
@@ -9,10 +9,11 @@ import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginResult
-import com.google.firebase.auth.FacebookAuthProvider
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidUserException
-import com.google.firebase.auth.FirebaseUser
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.*
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,16 +22,25 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var callbackManager: CallbackManager
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        auth = Firebase.auth
-        setTheme(R.style.AppThemeNoActionBar)
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppThemeNoActionBar)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
 
         // Initialize Facebook Login button
         callbackManager = CallbackManager.Factory.create()
         fbLoginButton.setPermissions("email", "public_profile")
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         addActivityListeners()
     }
@@ -43,12 +53,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        callbackManager.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Error al obtener credenciales de Usuario de Google: ${e}", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            callbackManager.onActivityResult(requestCode, resultCode, data)
+        }
+
     }
 
     private fun updateUI(user: FirebaseUser?){
         user?.let{
-            Toast.makeText(this, "El usuario Existe y es: ${it.email}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "El usuario Existe y es: ${if (user.isAnonymous) "anonimo" else user.email}", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -80,6 +103,22 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
+    private fun googleSignIn(){
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_SIGN_IN)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                updateUI(auth.currentUser)
+            } else {
+                Toast.makeText(this, "Error al crear Usuario de Google ${task.exception}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private fun handleFacebookAccessToken(token: AccessToken){
         val credential = FacebookAuthProvider.getCredential(token.token)
         auth.signInWithCredential(credential).addOnCompleteListener(this) { task ->
@@ -109,5 +148,27 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this@MainActivity, "Facebook Login Error", Toast.LENGTH_SHORT).show()
             }
         })
+
+        google_button.setOnClickListener {
+            googleSignIn()
+        }
+
+        textViewNoLogin.setOnClickListener {
+            anonymousSignIn()
+        }
+    }
+
+    private fun anonymousSignIn(){
+        auth.signInAnonymously().addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                updateUI(auth.currentUser)
+            }else{
+                Toast.makeText(this, "Error al crear Usuario Anonimo ${task.exception}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    companion object {
+        private const val GOOGLE_SIGN_IN = 9001
     }
 }
